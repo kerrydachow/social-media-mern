aaac1aa5055d3b38eff8b417c32714360b0b52de
kerrydachow <kerry_chow@hotmail.ca>
2023-07-06 20:20:51 -0700

    add post model, route, controller

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js"
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* PACKAGE CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url); // Grab file URL
@@ -41,8 +44,10 @@ const upload = multer({ storage });
 
 /* ROUTES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
