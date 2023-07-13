98103dc3815f5e267ca3318b7012b89f4dbc1de9
kerrydachow <kerry_chow@hotmail.ca>
2023-07-12 18:02:35 -0700

    Fix jsconfig to work with Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import jsconfigPaths from "vite-jsconfig-paths";
 
 // https://vitejs.dev/config/
+/**
+ * Use jsconfigPaths with Vite
+ * https://dev.to/larswaechter/path-aliases-with-typescript-in-nodejs-4353
+ */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), jsconfigPaths()],
   server: {
     port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
   },
 })
