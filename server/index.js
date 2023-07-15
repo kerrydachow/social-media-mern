fc4b2cc8eaeb7e44f6a7e13921dba26ac8137307
kerrydachow <kerry_chow@hotmail.ca>
2023-07-15 14:36:20 -0700

    Fix express.json middleware bug

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ dotenv.config(); // Use .env files
 const app = express();
 
 /* MIDDLEWARE */
-app.use(express.json);
+app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
