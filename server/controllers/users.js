b353581d8b8980a0540cdb232106e6feee094292
kerrydachow <kerry_chow@hotmail.ca>
2023-08-22 15:57:09 -0700

    Fix addRemove friend name bug

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -52,8 +52,8 @@ export const addRemoveFriend = async (req, res) => {
       user.friends.map((id) => User.findById(id))
     );
     const formattedFriends = friends.map(
-      ({ _id, fristName, lastName, occupation, location, picturePath }) => {
-        return { _id, fristName, lastName, occupation, location, picturePath };
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
     res.status(200).json(formattedFriends);
