102824c4f3cc9eb8acc822a6c6ca18599072d1d7
kerrydachow <kerry_chow@hotmail.ca>
2023-09-24 22:28:12 -0700

    Modify server to include comments route

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { ping } from "./controllers/ping.js";
@@ -52,6 +53,7 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+app.use("/comments", commentRoutes);
 app.use("/ping", ping);
 
 /* MONGOOSE SETUP */
